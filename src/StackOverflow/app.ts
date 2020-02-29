d22ac8890b89ac6246c9fdda90fa832f134f3dc6
Alvi <alvi.utsab@gmail.com>
2020-02-29 12:59:21 -0800

    adapt scraper to parse through all urls

--- src/StackOverflow/app.ts
+++ src/StackOverflow/app.ts
@@ -2,6 +2,6 @@ import { Scraper } from './controllers/Scraper'
 
 const scraper = new Scraper()
 scraper.scrape().then((data) => {
-    console.log(data)
+    console.log(JSON.stringify(data))
 })
 
--- src/StackOverflow/controllers/Scraper.ts
+++ src/StackOverflow/controllers/Scraper.ts
@@ -11,60 +11,65 @@ export class Scraper {
     constructor(){
         this.baseUrl = 'https://stackoverflow.com'
         this.tags = [
-            {
-                name: 'design-patterns',
-                url: 'https://stackoverflow.com/questions/tagged/design-patterns',
-                limit: 1880
-            },
+            // {
+            //     name: 'design-patterns',
+            //     url: 'https://stackoverflow.com/questions/tagged/design-patterns?tab=Newest',
+            //     limit: 1880
+            // },
             // {
             //     name: 'software-design',
-            //     url: 'https://stackoverflow.com/questions/tagged/software-design',
+            //     url: 'https://stackoverflow.com/questions/tagged/software-design?tab=Newest',
             //     limit: 97
             // },
             // {
             //     name: 'class-design',
-            //     url: 'https://stackoverflow.com/questions/tagged/class-design',
+            //     url: 'https://stackoverflow.com/questions/tagged/class-design?tab=Newest',
             //     limit: 70
             // },
             // {
             //     name: 'design-principles',
-            //     url: 'https://stackoverflow.com/questions/tagged/design-principles',
+            //     url: 'https://stackoverflow.com/questions/tagged/design-principles?tab=Newest',
             //     limit: 18
             // },
             // {
             //     name: 'system-design',
-            //     url: 'https://stackoverflow.com/questions/tagged/system-design',
+            //     url: 'https://stackoverflow.com/questions/tagged/system-design?tab=Newest',
             //     limit: 16
             // },
             // {
             //     name: 'application-design',
-            //     url: 'https://stackoverflow.com/questions/tagged/application-design',
+            //     url: 'https://stackoverflow.com/questions/tagged/application-design?tab=Newest',
             //     limit: 15
             // },
-            // {
-            //     name: 'code-design',
-            //     url: 'https://stackoverflow.com/questions/tagged/code-design',
-            //     limit: 11
-            // }
+            {
+                name: 'code-design',
+                url: 'https://stackoverflow.com/questions/tagged/code-design?tab=Newest',
+                limit: 11
+            }
         ]
     }
 
     public scrape = async () => {
         const data: any = []
         for(let tag of this.tags){
-            const indexPage = await new Fetcher(tag.url).fetchPage()
-            const questionSummary = await new IndexPage(indexPage).questionSummary()
-            for(let question of questionSummary.questions) {
-                const detailPage = await new Fetcher(this.baseUrl + question.href).fetchPage()
-                const questionDetail = await new DetailPage(detailPage).questionDetail()
-                const questionText: string = questionDetail.question.header + " " + questionDetail.question.body
-                const answerText: string = await questionDetail.answers.map((answer: { text: string }) => answer.text).join(" ")
-                const commentText: string = await questionDetail.comments.map((comment: {text: string}) => comment.text).join(" ")
-                data.push({
-                    question: questionText,
-                    answer: answerText,
-                    comment: commentText
-                })
+            let url = tag.url
+            while(url !== undefined && url !== null) {
+                console.log(`Parsing data from ${url}`)
+                const indexPage = await new Fetcher(url).fetchPage()
+                const questionSummary = await new IndexPage(indexPage).questionSummary()
+                for(let question of questionSummary.questions) {
+                    const detailPage = await new Fetcher(this.baseUrl + question.href).fetchPage()
+                    const questionDetail = await new DetailPage(detailPage).questionDetail()
+                    const questionText: string = questionDetail.question.header + " " + questionDetail.question.body
+                    const answerText: string = await questionDetail.answers.map((answer: { text: string }) => answer.text).join(" ")
+                    const commentText: string = await questionDetail.comments.map((comment: {text: string}) => comment.text).join(" ")
+                    data.push({
+                        question: questionText,
+                        answer: answerText,
+                        comment: commentText
+                    })
+                }
+                url = questionSummary.nextPageUrl === undefined ? null : this.baseUrl + questionSummary.nextPageUrl
             }
         }
         return data 
