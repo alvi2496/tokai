fc6923c464f07add98f07c3f1715a3197ee0d93f
Alvi <alvi.utsab@gmail.com>
2020-03-07 20:48:59 -0800

    add text processor to so data

--- src/StackOverflow/controllers/Scraper.ts
+++ src/StackOverflow/controllers/Scraper.ts
@@ -2,42 +2,49 @@ import * as data from '../data/tags.json'
 import { IndexPage } from "./IndexPage"
 import { DetailPage } from "./DetailPage"
 import { Fetcher } from "../../utils/Fetcher"
+import { TextProcessor } from '../../utils/TextProcessor'
 
 export class Scraper {
 
-    tags: any
     baseUrl: string
 
     constructor(){
         this.baseUrl = 'https://stackoverflow.com'
-        this.tags = data.tags
     }
 
     public scrape = async () => {
-        const data: any = []
-        for(let tag of this.tags){
-            let url = tag.url
-            while(url !== undefined && url !== null) {
-                console.log(`Parsing data from ${url}`)
-                const indexPage = await new Fetcher(url).fetchPage()
-                const questionSummary = await new IndexPage(indexPage).questionSummary()
-                for(let question of questionSummary.questions) {
-                    const detailPage = await new Fetcher(this.baseUrl + question.href).fetchPage()
-                    const questionDetail = await new DetailPage(detailPage).questionDetail()
-                    const questionText: string = questionDetail.question.header + " " + questionDetail.question.body
-                    const answerText: string = await questionDetail.answers.map((answer: { text: string }) => answer.text).join(" ")
-                    const commentText: string = await questionDetail.comments.map((comment: {text: string}) => comment.text).join(" ")
-                    data.push({
-                        url: question.href,
-                        question: questionText,
-                        answer: answerText,
-                        comment: commentText,
-                        tag: tag.name
-                    })
+        const rows: any = []
+        for(let category of data.categories) {
+            for(let tag of category.tags){
+                let url: any = tag.url
+                while(url !== undefined && url !== null) {
+                    console.log(`Parsing data from ${url}`)
+                    const indexPage = await new Fetcher(url).fetchPage()
+                    const questionSummary = await new IndexPage(indexPage).questionSummary()
+                    for(let question of questionSummary.questions) {
+                        const detailPage = await new Fetcher(this.baseUrl + question.href).fetchPage()
+                        const questionDetail = await new DetailPage(detailPage).questionDetail()
+                        let questionText: string = questionDetail.question.header + " " + questionDetail.question.body
+                        let answerText: string = await questionDetail.answers.map((answer: { text: string }) => answer.text).join(" ")
+                        let commentText: string = await questionDetail.comments.map((comment: {text: string}) => comment.text).join(" ")
+
+                        // process the texts
+                        questionText = await new TextProcessor().process(questionText)
+                        answerText = await new TextProcessor().process(answerText)
+                        commentText = await new TextProcessor().process(commentText)
+                        rows.push({
+                            url: question.href,
+                            question: questionText,
+                            answer: answerText,
+                            comment: commentText,
+                            tag: tag.name,
+                            label: category.label
+                        })
+                    }
+                    url = questionSummary.nextPageUrl === undefined ? null : this.baseUrl + questionSummary.nextPageUrl
                 }
-                url = questionSummary.nextPageUrl === undefined ? null : this.baseUrl + questionSummary.nextPageUrl
             }
         }
-        return data 
+        return rows
     }
 } 
