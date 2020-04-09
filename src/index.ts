975192ff00274f94c558dc6b30cfa44c12873328
Alvi <alvi.utsab@gmail.com>
2020-04-08 23:46:49 -0700

    add github module

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ let appName: string = 'StackOverflow'
 if(argv.app === 'so') appName = 'StackOverflow'
 else if(argv.app == 'pdf') appName = 'PdfDocument'
 else if(argv.app == 'csv') appName = 'csvDocument'
+else if(argv.app == 'gh') appName = 'github'
  
 require(`./${appName}/app`)
